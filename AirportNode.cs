ef8d9ab41efbedaea6d7c9c440d62675b103383f
Greg Prouty <Gregoryprouty@trentu.ca>
2022-01-30 18:13:06 -0500

    AirportNode and Main added
    AirportNode is mostly complete (ToString unfinished)
    Current state of Main is just for testing: to be changed upon
    completion of project

---
+++ AirportNode.cs
@@ -0,0 +1,46 @@
+﻿// Team: Greg Prouty, Bradley Primeau, Avery Chin
+using System;
+using System.Collections.Generic;
+
+class AirportNode
+{
+    public string Name { get; set; } //variable to store an Airports name
+    public string Code { get; set; } //variable to store the ID code of an Airport
+    public List<AirportNode> Destinations { get; set; } //variable containing the list of destinations for the Airport
+    public AirportNode(string name, string code) //Constructor taking a name and an id code as parameters
+    {
+        Name = name; //set Name to given name
+        Code = code; //set Code to given code
+        Destinations = new List<AirportNode>(); //initialize the list of destinations to an empty list
+    }
+    public void AddDestination(AirportNode destAirport) //method to add destinations to an Airports list of destinations
+    {
+        if (Destinations.Contains(destAirport)) //check if the destination already exists
+        {
+            Console.WriteLine("Target airport already has given airport as a destination."); //if destination already exists inform user that it cannot be added
+        }
+        else 
+        {
+            Destinations.Add(destAirport); //add destination to the target Airports list of destinations
+            Console.WriteLine("Airport added to targets list of destinations"); //inform the user that the destination was added
+        }
+    }
+
+    public void RemoveDestination(AirportNode destAirport) //Method to remove destinations from an Airports destinations list
+    {
+        if (!Destinations.Contains(destAirport)) //check if the target Airport has the parameter as a destination
+        {
+            Console.WriteLine("Removal target isn't a destination for that airport"); //if the destination isn't found inform the user it doesn't exist
+        }
+        else 
+        {
+            Destinations.Remove(destAirport); //remove the destination from the target destination list
+            Console.WriteLine("Target has been removed from that airports list of destinations"); //inform the user that the destination has been removed
+        }
+    }
+
+    public override string ToString()
+    {
+        return "Name: " + Name + " Code: " + Code;
+    }
+}
---
+++ TestMain.cs
@@ -0,0 +1,31 @@
+﻿// Team: Greg Prouty, Bradley Primeau, Avery Chin
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public static class TestMain 
+{
+    static void Main()
+    {
+        AirportNode node1 = new AirportNode("test1", "3000");
+        AirportNode node2 = new AirportNode("test2", "4000");
+        AirportNode node3 = new AirportNode("test3", "5000");
+        AirportNode node4 = new AirportNode("test4", "6000");
+        AirportNode node5 = new AirportNode("test5", "7000");
+        node1.AddDestination(node2);
+        node1.AddDestination(node3);
+        node1.AddDestination(node5);
+        node2.AddDestination(node1);
+        node1.AddDestination(node2);
+        node1.RemoveDestination(node2);
+        node1.RemoveDestination(node2);
+        node1.AddDestination(node2);
+        foreach (AirportNode i in node1.Destinations) 
+        {
+            Console.WriteLine(i.ToString());
+        }
+    }
+}
+
