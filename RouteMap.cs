f42342cac73ab801b41db8bb1b2cd17894dadc51
Megan-Risebrough <meganrisebrough24@gmail.com>
2022-02-05 21:15:54 -0500

    FastestRoute started and summaries added

--- RouteMap.cs
+++ RouteMap.cs
@@ -17,7 +17,11 @@ class RouteMap
     }// end RouteMap
 
     // FindAirportByName:
-    // Returns true if there exists an AirportNode within A that has the given name. Otherwise, false.
+    /// <summary>
+    /// Tells the user if an airport exists in the routemap by its name.
+    /// </summary>
+    /// <param name="name">The name of the airport to find</param>
+    /// <returns>True if there exists an AirportNode within A that has the given name. Otherwise, false.</returns>
     public bool FindAirportByName(string name)
     {
         foreach (AirportNode airport in A) // Look through each AirportNode in A
@@ -35,7 +39,11 @@ class RouteMap
     }// end FindAirportByName
 
     // FindAirportByCode:
-    // Returns true if there exists an AirportNode within A that has the given code. Otherwise, false.
+    /// <summary>
+    /// Tells the user if an airport exists in the routemap by its code.
+    /// </summary>
+    /// <param name="code">The code of the airport to find</param>
+    /// <returns>True if there exists an AirportNode within A that has the given code. Otherwise, false.</returns>
     public bool FindAirportByCode(string code)
     {
         foreach (AirportNode airport in A) // Look through each AirportNode in A
@@ -53,7 +61,11 @@ class RouteMap
     }// end FindAirportByCode
 
     // AddAirport:
-    // If AirportNode, a, does not already exist in List, A, then add it to A and return true. Otherwise, false.
+    /// <summary>
+    /// If AirportNode, a, does not already exist in List, A, then add it to A and return true. Otherwise, false.
+    /// </summary>
+    /// <param name="a">The airport to be added</param>
+    /// <returns>True if addition occurs, false if airport is not added</returns>
     public bool AddAirport(AirportNode a)
     {
         if (!A.Contains(a)) 
@@ -65,8 +77,12 @@ class RouteMap
     }// end AddAirport
 
     // RemoveAirport:
-    // If AirportNode, a, exists in List, A, then remove it from A and return true. Otherwise, false.
-    // If a is removed, then all routes to a will also be removed.
+    /// <summary>
+    /// If AirportNode, a, exists in List, A, then remove it from A and return true. Otherwise, false.
+    /// If a is removed, then all routes to a will also be removed.
+    /// </summary>
+    /// <param name="a">The airport to be removed</param>
+    /// <returns>True if removal occurs, false if airport is not removed</returns>
     public bool RemoveAirport(AirportNode a)
     {
         if (A.Contains(a))
@@ -87,7 +103,12 @@ class RouteMap
     }// end RemoveAirport
 
     // AddRoute:
-    // If origin and dest exist within List A, then add dest to origin's List of Destinations and return true. Otherwise, false.
+    /// <summary>
+    /// If origin and dest exist within List A, then add dest to origin's List of Destinations and return true. Otherwise, false.
+    /// </summary>
+    /// <param name="origin">The starting airport of type AirportNode</param>
+    /// <param name="dest">The target airport of type AirportNode</param>
+    /// <returns>True if addition occurs, false if route is not added</returns>
     public bool AddRoute(AirportNode origin, AirportNode dest)
     {
         if (A.Contains(origin) && A.Contains(dest))
@@ -102,7 +123,12 @@ class RouteMap
     }// end AddRoute
 
     // RemoveRoute:
-    // If origin and dest exist within List A, then remove dest from origin's List of Destinations and return true. Otherwise, false.
+    /// <summary>
+    /// If origin and dest exist within List A, then remove dest from origin's List of Destinations and return true. Otherwise, false.
+    /// </summary>
+    /// <param name="origin">The starting airport of type AirportNode</param>
+    /// <param name="dest">The target airport of type AirportNode</param>
+    /// <returns>True if removal occurs, false if route is not removed</returns>
     public bool RemoveRoute(AirportNode origin, AirportNode dest)
     {
         if (A.Contains(origin) && A.Contains(dest))
@@ -116,6 +142,55 @@ class RouteMap
         return false;
     }// end RemoveRoute
 
+    /// <summary>
+    /// Uses a breadth first search to find the shortest path between two given airports
+    /// </summary>
+    /// <param name="origin">The starting airport of type AirportNode</param>
+    /// <param name="dest">The target airport of type AirportNode</param>
+    /// <returns>A string containing the shortest route</returns>
+    public string FastestRoute (AirportNode origin, AirportNode dest)
+    {
+        Queue<AirportNode> frontierQueue = new Queue<AirportNode>(); // Queue used to move through the AirportNodes
+        List<AirportNode> discoveredSet = new List<AirportNode>(); // List for AirportNodes once they've been discovered
+        AirportNode currentAirport; // Used to keep track of the current AirportNode
+        string route = ""; // String to be added to and returned containing the route
+
+        frontierQueue.Enqueue(origin); // Enqueue the starting AirportNode
+        discoveredSet.Add(origin); // Add the starting airport to the discovered ones
+
+        while (frontierQueue.Count > 0) // While there is another airport to check
+        {
+            currentAirport = frontierQueue.Dequeue(); 
+
+            if (currentAirport == null)
+            {
+                return "An error occurred";
+            }
+
+            if (currentAirport == dest) // If the current airport is the one we want, stop and return the route
+            {
+                route = route + currentAirport.Name + ":" + currentAirport.Code;
+                return route;
+            }
+            else
+            {
+                route = route + currentAirport.Name + ":" + currentAirport.Code + " -> "; // Add the current airport to the route
+                foreach (AirportNode adjA in currentAirport.Destinations) { // Check each destination airport from current airport
+                    if (!(discoveredSet.Contains(adjA))) // If destination airport has not been discovered, discover it
+                    {
+                        frontierQueue.Enqueue(adjA);
+                        discoveredSet.Add(adjA);
+                    }
+                }
+            }
+        }
+        return "An error occurred";
+    }// End FastestRoute
+
+    /// <summary>
+    /// Used to print out the route map in a nice format.
+    /// </summary>
+    /// <returns>A string listing the airports in the routemap an their destinations</returns>
     public override string ToString()
     {
         string airportList = "";
